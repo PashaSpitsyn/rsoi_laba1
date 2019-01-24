418b3d10b0038c06c902631ac6635beeb29e2ffc
Павел <spitsyn.p.s@yandex.ru>
2019-01-25 01:38:41 +0300

    laba 1

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">By Spitsyn Pavel IU7-12M</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          For laba 1
         </p>
       </div>
     );
